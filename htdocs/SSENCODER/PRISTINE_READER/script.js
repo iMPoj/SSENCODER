5a648659fa574fd6b23142c667cab92586d6bdcb
John Lloyd L. Pojadas <116985046+iMPoj@users.noreply.github.com>
2026-07-01 15:30:51 +0800

    Delete htdocs/SSENCODER directory

--- htdocs/SSENCODER/PRISTINE_READER/script.js
+++
@@ -1,5 +0,0 @@
-function filterData() {
-    var location = document.getElementById('location').value;
-    var dataType = document.getElementById('dataType').value;
-    window.location.href = 'viewer.php?location=' + location + '&dataType=' + dataType;
-}
